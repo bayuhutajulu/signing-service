074fb7e8bcd11e5d8830a00b4fd6781f6f280397
bayuhutajulu <bayuhutajulu@gmail.com>
2025-10-29 12:57:10 +0700

    implement signer

--- crypto/signer.go
+++ crypto/signer.go
@@ -1,8 +1,44 @@
 package crypto
 
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+)
+
 // Signer defines a contract for different types of signing implementations.
 type Signer interface {
 	Sign(dataToBeSigned []byte) ([]byte, error)
 }
 
-// TODO: implement RSA and ECDSA signing ...
+type RSASigner struct {
+	privateKey *rsa.PrivateKey
+}
+
+func NewRSASigner(privateKey *rsa.PrivateKey) *RSASigner {
+	return &RSASigner{
+		privateKey: privateKey,
+	}
+}
+
+func (s *RSASigner) Sign(dataTobeSigned []byte) ([]byte, error) {
+	hash := sha256.Sum256(dataTobeSigned)
+	return rsa.SignPKCS1v15(rand.Reader, s.privateKey, crypto.SHA256, hash[:])
+}
+
+type ECDSASigner struct {
+	privateKey *ecdsa.PrivateKey
+}
+
+func NewECDSASigner(privateKey *ecdsa.PrivateKey) *ECDSASigner {
+	return &ECDSASigner{
+		privateKey: privateKey,
+	}
+}
+
+func (s *ECDSASigner) Sign(dataTobeSigned []byte) ([]byte, error) {
+	hash := sha256.Sum256(dataTobeSigned)
+	return ecdsa.SignASN1(rand.Reader, s.privateKey, hash[:])
+}
